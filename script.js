9f47ef00ce0007d70ad8a8e543b4001735acb17a
Eccie-K <estherkc100@gmail.com>
2019-05-05 21:48:35 +0300

    added jquery and javascript functions

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("#show").click(function(){
+        $(".design").toggle("slide");
+    });
+});
+
+
+$(document).ready(function(){
+    $("#show").click(function(){
+        $(".development").toggle("slide");
+    });
+});
+
+$(document).ready(function(){
+    $("#show").click(function(){
+        $(".product").toggle("slide");
+    });
+});
