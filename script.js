19a8af45c13add8c6944c039b9e65a1f208f8df2
Eccie-K <estherkc100@gmail.com>
2019-05-06 10:21:03 +0300

    added button and alert function on form

--- script.js
+++ script.js
@@ -6,14 +6,27 @@ $(document).ready(function(){
 
 
 $(document).ready(function(){
-    $("#show").click(function(){
+    $("#click").click(function(){
         $(".development").toggle("slide");
     });
 });
 
 $(document).ready(function(){
-    $("#show").click(function(){
+    $("#s").click(function(){
         $(".product").toggle("slide");
     });
 });
 
+
+("button").click(function() {
+    var client = document.getElementById('nameDetail').value;
+    alert(' Dear ' + ' esteemed ' + client + ' we have received your message successful. ' + ' Feel free to reach out to us anytime');
+    event.preventDefault();
+});
+//    reset my form
+  $("button").on('click', function(){
+    $('form').each(function(){
+      this.reset();
+      alert(client);
+    });
+});
